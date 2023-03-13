e392c34d56b4b60b1276bc1449689a295dc4eaaa
Asa <94348484+AzzamSyakir@users.noreply.github.com>
2023-03-13 11:22:48 +0700

    Update HomeController.php

--- app/Http/Controllers/Barang/HomeController.php
+++ app/Http/Controllers/Barang/HomeController.php
@@ -17,13 +17,21 @@ class HomeController extends Controller
         ]);
         try {
             //foreign key hanya bisa diisi oleh user dengan tipe seller
-            $user = User::where('tipe', 's')->firstOrFail();
-            $barang = new Barang;
-            $barang->nama_barang = $data['nama_barang'];
-            $barang->harga = $data['harga'];
-            $barang->user_id = $user->id;
-            $barang->save();
-            return response()->json($barang);
+            $user = User::where('tipe', 's')->first();
+            if ($user !== null) {
+                $barang = new Barang;
+                $barang->nama_barang = $data['nama_barang'];
+                $barang->harga = $data['harga'];
+                $barang->user_id = $user->id;
+                $barang->save();
+                return response()->json($barang);
+            } else {
+                //return bila tidak ada user sellere 
+                return response()->json([
+                    'message' => 'Tidak ada tipe user penjual',
+                    'error' => 'User penjual tidak ditemukan'
+                ], 404);
+            }
         } catch (\Throwable $th) {
             return response()->json([
                 'message' => 'Gagal Tambah Barang',
